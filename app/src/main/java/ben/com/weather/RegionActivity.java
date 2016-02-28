b394db3bf1446bb8ce06893b93307b97d177602c
Ben Munro <benjamin.munro@gmail.com>
2016-02-28 16:36:03 +1100

    Indent with tabs, not spaces

--- app/src/main/java/ben/com/weather/RegionActivity.java
+++ app/src/main/java/ben/com/weather/RegionActivity.java
@@ -35,8 +35,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class RegionActivity extends Activity implements SwipeRefreshLayout.OnRefreshListener {
-    private static final String DEBUG_TAG = "WeatherAppLog";
-    private static final List<String> columns = Arrays.asList(
+	private static final String DEBUG_TAG = "WeatherAppLog";
+	private static final List<String> columns = Arrays.asList(
 		"-datetime",
 		"-temp",
 		"-apptemp",
@@ -68,16 +68,16 @@ public class RegionActivity extends Activity implements SwipeRefreshLayout.OnRef
 	private SwipeRefreshLayout swipeLayout;
 
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_region);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_region);
 
 		swipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
 		swipeLayout.setOnRefreshListener(this);
 
 		main();
 
-    }
+	}
 
 	@Override
 	public void onRefresh() {
@@ -119,55 +119,55 @@ public class RegionActivity extends Activity implements SwipeRefreshLayout.OnRef
 		}
 	}
 
-    private void displayData(ArrayList<HashMap<String,String>> region) {
-	    Resources r = getResources();
-	    int locationWidth = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 120, r.getDisplayMetrics());
-	    int cellWidth = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, r.getDisplayMetrics());
+	private void displayData(ArrayList<HashMap<String,String>> region) {
+		Resources r = getResources();
+		int locationWidth = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 120, r.getDisplayMetrics());
+		int cellWidth = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, r.getDisplayMetrics());
 
-	    TableLayout tl = (TableLayout) findViewById(R.id.regiontable);
-	    TableRow header = new TableRow(this);
-	    TextView locationHeading = new TextView(this);
-	    locationHeading.setText("Station");
-	    header.addView(locationHeading);
+		TableLayout tl = (TableLayout) findViewById(R.id.regiontable);
+		TableRow header = new TableRow(this);
+		TextView locationHeading = new TextView(this);
+		locationHeading.setText("Station");
+		header.addView(locationHeading);
 
-	    for (String column : displayColumns.keySet()) {
-		    TextView columnHeading = new TextView(this);
-		    columnHeading.setText(displayColumns.get(column));
-		    header.addView(columnHeading);
-	    }
-	    tl.addView(header);
+		for (String column : displayColumns.keySet()) {
+			TextView columnHeading = new TextView(this);
+			columnHeading.setText(displayColumns.get(column));
+			header.addView(columnHeading);
+		}
+		tl.addView(header);
 
-	    int row = 0;
+		int row = 0;
 
-	    for (HashMap<String,String> observations : region) {
-		    TableRow tr = new TableRow(this);
+		for (HashMap<String,String> observations : region) {
+			TableRow tr = new TableRow(this);
 
-		    if (row % 2 == 0) {
-			    tr.setBackgroundResource(R.color.teal50);
-		    } else {
-			    tr.setBackgroundResource(R.color.teal100);
-		    }
+			if (row % 2 == 0) {
+				tr.setBackgroundResource(R.color.teal50);
+			} else {
+				tr.setBackgroundResource(R.color.teal100);
+			}
 
-		    Log.d(DEBUG_TAG, "observations: " + observations.toString());
-		    TextView location = new TextView(this);
-		    location.setText(observations.get("station"));
-		    location.setWidth(locationWidth);
-		    tr.addView(location);
+			Log.d(DEBUG_TAG, "observations: " + observations.toString());
+			TextView location = new TextView(this);
+			location.setText(observations.get("station"));
+			location.setWidth(locationWidth);
+			tr.addView(location);
 
-		    Log.d(DEBUG_TAG, "station: " + observations.get("station"));
-		    for (String column : displayColumns.keySet()) {
-			    Log.d(DEBUG_TAG, "station: " + column + " = " + observations.get(column));
-			    TextView cell = new TextView(this);
-			    cell.setText(observations.get(column));
-			    cell.setWidth(cellWidth);
-			    tr.addView(cell);
-		    }
+			Log.d(DEBUG_TAG, "station: " + observations.get("station"));
+			for (String column : displayColumns.keySet()) {
+				Log.d(DEBUG_TAG, "station: " + column + " = " + observations.get(column));
+				TextView cell = new TextView(this);
+				cell.setText(observations.get(column));
+				cell.setWidth(cellWidth);
+				tr.addView(cell);
+			}
 
-		    tl.addView(tr);
+			tl.addView(tr);
 
-		    row++;
-	    }
-    }
+			row++;
+		}
+	}
 
 	private class DownloadRegionObsTask extends AsyncTask<Void, Void, ArrayList<HashMap<String,String>>> {
 		String url;
@@ -178,44 +178,44 @@ public class RegionActivity extends Activity implements SwipeRefreshLayout.OnRef
 			this.htmlFormat = htmlFormat;
 		}
 
-        @Override
-        protected ArrayList<HashMap<String,String>> doInBackground(Void... params) {
+		@Override
+		protected ArrayList<HashMap<String,String>> doInBackground(Void... params) {
 
-            ArrayList region = new ArrayList<HashMap<String,String>>();
+			ArrayList region = new ArrayList<HashMap<String,String>>();
 
-            try {
-                Document document = Jsoup.connect(this.url).get();
-                Elements rows = document.select("tr.rowleftcolumn");
-                for (Element stationRow : rows) {
-                    HashMap<String,String> observations = new HashMap<String,String>();
-                    
-                    observations.put("station", stationRow.select("a").html());
+			try {
+				Document document = Jsoup.connect(this.url).get();
+				Elements rows = document.select("tr.rowleftcolumn");
+				for (Element stationRow : rows) {
+					HashMap<String,String> observations = new HashMap<String,String>();
 
-	                JSONObject jo = loadConfig(R.raw.regionhtmlformat);
+					observations.put("station", stationRow.select("a").html());
 
-                    for (String column : columns) {
-                        String observation = stationRow.select("[headers*=\"" + column + "\"]").html();
-                        observations.put(column, observation);
-                    }
+					JSONObject jo = loadConfig(R.raw.regionhtmlformat);
 
-                    region.add(observations);
-                }
+					for (String column : columns) {
+						String observation = stationRow.select("[headers*=\"" + column + "\"]").html();
+						observations.put(column, observation);
+					}
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+					region.add(observations);
+				}
 
-            return region;
-        }
-	    
-        // onPostExecute displays the results of the AsyncTask.
-        @Override
-        protected void onPostExecute(ArrayList<HashMap<String,String>> region) {
-            Log.d(DEBUG_TAG, "DATA: " + region.toString());
-            displayData(region);
-	        swipeLayout.setRefreshing(false);
-        }
-    }
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			return region;
+		}
+
+		// onPostExecute displays the results of the AsyncTask.
+		@Override
+		protected void onPostExecute(ArrayList<HashMap<String,String>> region) {
+			Log.d(DEBUG_TAG, "DATA: " + region.toString());
+			displayData(region);
+			swipeLayout.setRefreshing(false);
+		}
+	}
 
 	// TODO: Do this in a Singleton class or something so that we don't keep reloading it.
 	public JSONObject loadConfig(int configId) {
